72544176df1d85bb02f5a3919388001f1ea39d20
Alexander Mamani Yucra <akeymy4@gmail.com>
2022-02-05 01:06:03 -0400

    feat: add CRUD passenger

--- src/passenger/passenger.controller.ts
+++ src/passenger/passenger.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { IPassenger } from 'src/commons/interfaces/passenger.interface';
+import { PassengerDTO } from './dto/passenger.dto';
+import { PassengerService } from './passenger.service';
 
-@Controller('passenger')
-export class PassengerController {}
+@Controller('api/v1/passenger')
+export class PassengerController {
+
+    constructor(private readonly passengerService: PassengerService){}
+
+    @Post()
+    create(@Body() passengerDTO: PassengerDTO): Promise<IPassenger>{
+        return this.passengerService.create(passengerDTO);
+    }
+
+    @Get()
+    findAll(): Promise<IPassenger[]>{
+        return this.passengerService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string): Promise<IPassenger>{
+        return this.passengerService.findOne(id);
+    }
+
+    @Put(':id')
+    update(@Param('id') id: string, @Body() passegerDTO: PassengerDTO): Promise<IPassenger>{
+        return this.passengerService.update(id, passegerDTO);
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id: string) {
+        return this.passengerService.delete(id);
+    }
+
+}
