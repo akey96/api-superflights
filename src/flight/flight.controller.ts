c59c4e7c167195913142727c09fcb0a915af7deb
Alexander Mamani Yucra <akeymy4@gmail.com>
2022-02-05 02:42:22 -0400

    feat: export module Passenger to Module Flght, create CRUD for module Flight and create endopoint for add passenger to flight

--- src/flight/flight.controller.ts
+++ src/flight/flight.controller.ts
@@ -1,4 +1,54 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpException, HttpStatus, Param, Post, Put } from '@nestjs/common';
+import { PassengerService } from 'src/passenger/passenger.service';
+import { FlightDTO } from './dto/flight.dto';
+import { FlightService } from './flight.service';
 
-@Controller('flight')
-export class FlightController {}
+@Controller('api/v1/flight')
+export class FlightController {
+
+    constructor(
+        private readonly flightService: FlightService,
+        private readonly passengerService: PassengerService
+        ){}
+
+    @Post()
+    create(@Body() flightDTO: FlightDTO): Promise<FlightDTO> {
+        return this.flightService.create(flightDTO);
+    }
+
+    @Get()
+    findAll(): Promise<FlightDTO[]> {
+        return this.flightService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string ): Promise<FlightDTO> {
+        return this.flightService.findOne(id);
+    }
+
+    @Put(':id')
+    update(@Param('id') id: string, @Body() flightDTO: FlightDTO ): Promise<FlightDTO> {
+        return this.flightService.update(id, flightDTO);
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id: string) {
+        return this.flightService.delete(id);
+    }
+
+    @Post(':flightId/passenger/:passengerId')
+    async addPassenger(
+        @Param('flightId') flightId: string,
+        @Param('passengerId') passengerId: string
+        ) {
+        
+        const passenger = await this.passengerService.findOne(passengerId);
+        if(!passenger){
+            throw new HttpException(`Passenger Not Found`, HttpStatus.NOT_FOUND);
+        }
+        return this.flightService.addPassenger(flightId, passengerId);
+        
+    }
+
+
+}
--- src/flight/flight.service.ts
+++ src/flight/flight.service.ts
@@ -1,4 +1,54 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { IFlight } from 'src/commons/interfaces/flight.interface';
+import { FLIGHT, PASSENGER } from 'src/commons/models/models';
+import { FlightDTO } from './dto/flight.dto';
 
 @Injectable()
-export class FlightService {}
+export class FlightService {
+
+    constructor(
+        @InjectModel(FLIGHT.name)
+        private readonly model: Model<IFlight>
+    ){}
+
+    async create(flightDTO: FlightDTO): Promise<IFlight> {
+        const newFlight = new this.model(flightDTO);
+        return await newFlight.save()
+    }
+
+    async findAll(): Promise<IFlight[]> {
+        return await this.model.find()
+        .populate(PASSENGER.name);
+    }
+
+    async findOne(id: string): Promise<IFlight> {
+        return await this.model.findById(id)
+        .populate(PASSENGER.name);
+    }
+
+    async update(id: string, flightDTO: FlightDTO): Promise<IFlight> {
+        return await this.model.findByIdAndUpdate(id, flightDTO, {new: true});
+    }
+
+    async delete(id: string) {
+        await this.model.findByIdAndDelete(id);
+        return {
+            status: HttpStatus.OK,
+            msg: 'Deleted'
+        };
+    }
+
+    async addPassenger(flightId: string, passengerId: string): Promise<IFlight>{
+        return await this.model.findByIdAndUpdate(
+            flightId,
+            {
+                $addToSet: {passengers: passengerId}
+            },
+            {new: true}
+        )
+        .populate(PASSENGER.name);
+    }
+    
+}
