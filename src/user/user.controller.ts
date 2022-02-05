fc21201b3838d22b1b7feefccf445685b38ca2c8
Alexander Mamani Yucra <akeymy4@gmail.com>
2022-02-05 00:10:40 -0400

    feat: create method find, findAll, update and delete for controllers of users

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { IUser } from 'src/commons/interfaces/user.interface';
 import { UserDTO } from './dto/user.dto';
 import { UserService } from './user.service';
 
@@ -10,5 +11,25 @@ export class UserController {
     @Post()
     create(@Body() body: UserDTO){
         return this.userService.create(body);
+    }
+
+    @Get()
+    findAll(){
+        return this.userService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string): Promise<IUser>{
+        return this.userService.findOne(id);
+    }
+
+    @Put(':id')
+    update(@Param('id') id: string, @Body() userDTO: UserDTO): Promise<IUser>{
+        return this.userService.update(id, userDTO);
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id: string){
+        return this.userService.delete(id);
     }
 }
