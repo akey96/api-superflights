24510957513791a4956e4ec6afb7e0b5d32ee903
Alexander Mamani Yucra <akeymy4@gmail.com>
2022-02-04 23:45:07 -0400

    build: install dependency bcrypt

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { USER } from 'src/commons/models/models';
+import { Userchema } from './schema/user.schema';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
+  imports: [
+    MongooseModule.forFeatureAsync([
+      {
+        name: USER.name,
+        useFactory: () => {
+          return Userchema;
+        }
+      }
+    ])
+  ],
   controllers: [UserController],
   providers: [UserService]
 })
